15d6cb45952b278e7dd27c21c72200ef868d002e
KashishMaurya <kashish.social.18@gmail.com>
2025-07-17 16:59:04 +0530

    render error fix

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Serve static files (uploaded images)
@@ -31,9 +31,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, "0.0.0.0", () =>
-      console.log("Server running on 0.0.0.0:5000")
-    );
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error("MongoDB connection failed:", err.message);
