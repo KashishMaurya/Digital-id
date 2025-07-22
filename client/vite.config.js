f896e873cac5e7f22ab74df6b9d8cf898a355021
KashishMaurya <kashish.social.18@gmail.com>
2025-07-22 21:28:28 +0530

    cors fix vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // your backend server
+        target: "https://care-connect-iq7u.onrender.com", //backend server
         changeOrigin: true,
         secure: false,
       },
